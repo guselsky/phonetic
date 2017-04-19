56d263ca0139efb0330681157252efacdc6c53a9
David <dvdgsl@gmail.com>
2017-04-19 12:36:55 +0200

    added setup for regular expressions

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,24 @@
 function resultInput(inputText) {	
-	input = inputText.inputbox.value;
-	alert(input);
+	input = tdransform(inputText.inputbox.value);
+	$('#result-area').text(output);
+}
+
+function tdransform(input) {
+
+	firstLetter = capitals(input.charAt(0));
+	otherLetters = miniscules(input.substr(1));
+
+	output = firstLetter + otherLetters;
+	return output;
+}
+
+function capitals(capital){
+	capital = capital.replace(/[D,d,T,t]/, 'D.T' );
+	capital = capital.replace(/[J,j,I,i]/, 'I.J' );
+
+	return capital;
+}
+
+function miniscules(miniscule) {
+	return miniscule;
 }
