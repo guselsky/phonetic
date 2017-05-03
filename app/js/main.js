077db5f3d74cb2fa92b959463bac4a886464e3a4
David <dvdgsl@gmail.com>
2017-05-03 14:16:36 +0200

    Enter button now triggers conversion

--- app/js/main.js
+++ app/js/main.js
@@ -1,24 +1,113 @@
 function resultInput(inputText) {	
-	input = tdransform(inputText.inputbox.value);
+	
+	var input = tdransform($('#submit-field').val().toLowerCase());
+
 	$('#result-area').text(output);
+
 }
 
+// Trigger transformation on Pressing Enter
+$('#submit-field').keyup(function(event) {
+	if (event.keyCode == 13) {
+		$('#result').click();
+	}
+});
+
+
 function tdransform(input) {
+	firstTwoletters = input.substr(0,2);
+	var doubleLetters = new RegExp(/(äu)|(aj)|(ej)|(eu)|(ay)|(ey)|(ä)|(ae)|(ö)|(oe)/);
+	firstThreeletters = input.substr(0,3);
+	var tripleLetters = new RegExp(/sch/);
 
-	firstLetter = capitals(input.charAt(0));
-	otherLetters = miniscules(input.substr(1));
-
-	output = firstLetter + otherLetters;
+	if(doubleLetters.test(firstTwoletters)) {
+		firstLetters = input.substr(0,2);
+		otherLetters = input.substr(2);
+	} else if (tripleLetters.test(firstThreeletters)) {
+		firstLetters = input.substr(0,3);
+		otherLetters = input.substr(3);
+	} else {
+		firstLetters = input.charAt(0);
+		otherLetters = input.substr(1);
+	} 
+	
+	output = capitals(firstLetters) + ' ' +  miniscules(otherLetters);
 	return output;
 }
 
-function capitals(capital){
-	capital = capital.replace(/[D,d,T,t]/, 'D.T' );
-	capital = capital.replace(/[J,j,I,i]/, 'I.J' );
+	var eiGruppe = new RegExp (/(äu)|(aj)|(ej)|(eu)|(ay)|(ey)/);
+	var eGruppe = new RegExp(/(ä)|(ae)|(ö)|(oe)/);
+	var singlesGroup = {
+	singlesExpression: [/d|t/,/j|i/,/b|p/,/c|g|k/,/f|v|w/],
+	replacement: ['D.T','I.J','B.P','C.G.K','F.V.W']
+	};
 
+function capitals(capital){
+
+
+
+	if (capital.length === 3) {
+		return capital = capital.replace(/(sch)/, 'S.SCH' );
+	} else if (eiGruppe.test(capital)) {
+		return capital = capital.replace(eiGruppe, 'Ei' );
+	} else if (eGruppe.test(capital)){
+		return	capital = capital.replace(eGruppe, 'E' ); 
+
+	} else {
+
+		for (var i = 0; i <=singlesGroup.singlesExpression.length; i++) {
+			capital = capital.replace(singlesGroup.singlesExpression[i], singlesGroup.replacement[i]);			
+		}
+
+	}
+
+	if (capital.length === 1) {
+		capital = capital.toUpperCase();
+	}
+	
 	return capital;
 }
 
 function miniscules(miniscule) {
-	return miniscule;
+
+	// take word and split into an array of letters
+
+	var minisculeArray = miniscule.split('');
+
+	// Sort out double vowels
+
+	var vowels = new RegExp(/(a)|(e)|(i)|(o)|(u)/);
+
+	for (var i = 0; i < minisculeArray.length; i++) {
+		// if (minisculeArray[i] === 'a') {
+			if(vowels.test(minisculeArray[i]) && vowels.test(minisculeArray[i+1])){
+			
+			minisculeArray.splice(i + 1, 1);	
+		}
+	}
+
+	//Check if is qu and make an exception
+
+	// set expressions group
+
+	// check if h is silent
+
+	// check if double consonants and remove them
+
+	// check if c i k or z sound and change accordingly
+
+	// check if ph sounds like f and change accordingly
+
+	// check if consonants gs, ks, chs, cks are one sound and change accordingly
+
+	// change cs, cZ, Rs, Rz, Zs
+
+	// check if the c in sc, schc is pronunced like a k
+
+	// change Sch, Sz, Scs, Scz, Szc, Szs, Szcs, Szcz
+
+
+	// execute transformation
+
+	return minisculeArray.join('');
 }
